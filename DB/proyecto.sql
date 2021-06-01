d8f2a5caf954ddb7b2f393c523f23a913b3b99b5
mupe990216 <emunozp1400@alumno.ipn.mx>
2021-06-01 03:33:42 -0500

    ER V2

--- DB/proyecto.sql
+++ DB/proyecto.sql
@@ -2,219 +2,106 @@ drop database if exists prueba;
 create database prueba;
 use prueba;
 
-create table if not exists persona(
+create table if not exists Credencial(
+  Usuario varchar(64) not null,
+  Contrasenia varchar(64) not null,
+  primary key(Usuario,Contrasenia)
+);
+
+create table if not exists TipoUsuario(
+  idTipoUsuario int(2) not null primary key,
+  Descripcion varchar(64) not null
+);
+
+insert into TipoUsuario values(1,'Administrador');
+insert into TipoUsuario values(2,'Cliente');
+
+create table if not exists Persona(
   Email varchar(64) not null primary key,
   Nombre varchar(64) not null,
   ApePaterno varchar(64) not null,
   ApeMaterno varchar(64) not null,
   Usuario varchar(64) not null,
   Contrasenia varchar(64) not null,
-  
+  idTipoUsuario int(2) not null,
+  foreign key (Usuario) references Credencial(Usuario) on delete cascade on update cascade,
+  foreign key (Contrasenia) references Credencial(Contrasenia) on delete cascade on update cascade,
+  foreign key (idTipoUsuario) references TipoUsuario(idTipoUsuario) on delete cascade on update cascade
+);
+
+-- Validar un SP para que solo un usuario del tipo 1 
+-- Pueda agregar info a esta tabla
+create table if not exists Gestiona_Persona(
+  idRegistro int(16) not null primary key,
+  Email_Admin varchar(64) not null,
+  Email_Modificado  varchar(64) not null,
+  FechaRegistro timestamp default current_timestamp,
+  foreign key (Email_Admin) references Persona(Email) on delete cascade on update cascade,
+  foreign key (Email_Modificado) references Persona(Email) on delete cascade on update cascade
+);
+
+create table if not exists Cat_Publicacion(
+  idCategoria int(2) not null primary key,
+  Descripcion varchar(64) not null
+);
+
+insert into Cat_Publicacion values(1,'Alimento');
+insert into Cat_Publicacion values(2,'Comportamiento');
+insert into Cat_Publicacion values(3,'Adiestramiento');
+insert into Cat_Publicacion values(4,'Juguetes');
+insert into Cat_Publicacion values(5,'Salud');
+
+create table if not exists Publicacion(
+  idModulo int(16) not null primary key,
+  Titulo varchar(64) not null,
+  Descripcion varchar(2048) not null,
+  Imagen varchar(128),
+  idCategoria int(2) not null,
+  foreign key (idCategoria) references Cat_Publicacion(idCategoria) on delete cascade on update cascade
+);
+
+-- Validar un SP para que solo un usuario del tipo 1 
+-- Pueda agregar info a esta tabla
+create table if not exists Gestiona_Publicacion(
+  idRegistro int(16) not null primary key,
+  Email_Admin varchar(64) not null,
+  idPublicacion int(16) not null,
+  FechaRegistro timestamp default current_timestamp,
+  foreign key (Email_Admin) references Persona(Email) on delete cascade on update cascade,
+  foreign key (idPublicacion) references Publicacion(idModulo) on delete cascade on update cascade
+);
+
+create table if not exists Foro(
+  idForo int(64) not null primary key,
+  Titulo varchar(64) not null,
+  Descripcion varchar(2048) not null,
+  Email_Admin varchar(64) not null,
+  foreign key (Email_Admin) references Persona(Email) on delete cascade on update cascade
+);
+
+-- Validar un SP para que solo un usuario del tipo 1 
+-- Pueda agregar info a esta tabla
+create table if not exists Gestiona_Foro(
+  idRegistro int(16) not null primary key,
+  Email_Admin varchar(64) not null,
+  idForo int(64) not null,
+  FechaRegistro timestamp default current_timestamp,
+  foreign key (Email_Admin) references Persona(Email) on delete cascade on update cascade,
+  foreign key (idForo) references Foro(idForo) on delete cascade on update cascade
+);
+
+create table if not exists Mensaje(
+  idMsj int(64) not null,
+  idForo int(64) not null,
+  Descripcion varchar(2048) not null,
+  Email_Cliente varchar(64) not null,
+  foreign key (Email_Cliente) references Persona(Email) on delete cascade on update cascade,
+  foreign key (idForo) references Foro(idForo) on delete cascade on update cascade,
+  primary key (idMsj,idForo)
 );
 
 
-CREATE SCHEMA IF NOT EXISTS `prueba` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
-USE `prueba` ;
-
--- -----------------------------------------------------
--- Table `prueba`.`administrador`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `prueba`.`administrador` (
-  `nombre` VARCHAR(45) NOT NULL,
-  `correoAdmin` VARCHAR(45) NOT NULL,
-  `contraseña` VARCHAR(45) NOT NULL,
-  `Gestiona_correoAdmin` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`correoAdmin`),
-  INDEX `FK6` (`Gestiona_correoAdmin` ASC) VISIBLE,
-  CONSTRAINT `FK6`
-    FOREIGN KEY (`Gestiona_correoAdmin`)
-    REFERENCES `prueba`.`administrador` (`correoAdmin`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
 
 
--- -----------------------------------------------------
--- Table `prueba`.`modulo_info`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `prueba`.`modulo_info` (
-  `id_modulo` INT NOT NULL,
-  `titulo` VARCHAR(45) NOT NULL,
-  `descripcion` VARCHAR(45) NOT NULL,
-  `imagenes` INT NOT NULL,
-  PRIMARY KEY (`id_modulo`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
 
 
--- -----------------------------------------------------
--- Table `prueba`.`categoria`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `prueba`.`categoria` (
-  `id_categoria` INT NOT NULL,
-  `descripción_` VARCHAR(45) NOT NULL,
-  `id_modulo` INT NOT NULL,
-  PRIMARY KEY (`id_categoria`),
-  INDEX `FK1` (`id_modulo` ASC) VISIBLE,
-  CONSTRAINT `FK1`
-    FOREIGN KEY (`id_modulo`)
-    REFERENCES `prueba`.`modulo_info` (`id_modulo`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `prueba`.`cliente`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `prueba`.`cliente` (
-  `contraseña` VARCHAR(45) NOT NULL,
-  `nombre` VARCHAR(45) NOT NULL,
-  `correoCliente` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`correoCliente`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `prueba`.`foro`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `prueba`.`foro` (
-  `autor` VARCHAR(45) NOT NULL,
-  `titulo` VARCHAR(45) NOT NULL,
-  `descripción_` VARCHAR(45) NOT NULL,
-  `id_foro` INT NOT NULL,
-  PRIMARY KEY (`id_foro`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `prueba`.`controla`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `prueba`.`controla` (
-  `correoAdmin` VARCHAR(45) NOT NULL,
-  `id_foro` INT NOT NULL,
-  PRIMARY KEY (`correoAdmin`, `id_foro`),
-  INDEX `FK10` (`id_foro` ASC) VISIBLE,
-  CONSTRAINT `FK10`
-    FOREIGN KEY (`id_foro`)
-    REFERENCES `prueba`.`foro` (`id_foro`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-  CONSTRAINT `FK9`
-    FOREIGN KEY (`correoAdmin`)
-    REFERENCES `prueba`.`administrador` (`correoAdmin`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `prueba`.`gestionacliente`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `prueba`.`gestionacliente` (
-  `correoCliente` VARCHAR(45) NOT NULL,
-  `correoAdmin` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`correoCliente`, `correoAdmin`),
-  INDEX `FK8` (`correoAdmin` ASC) VISIBLE,
-  CONSTRAINT `FK7`
-    FOREIGN KEY (`correoCliente`)
-    REFERENCES `prueba`.`cliente` (`correoCliente`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-  CONSTRAINT `FK8`
-    FOREIGN KEY (`correoAdmin`)
-    REFERENCES `prueba`.`administrador` (`correoAdmin`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `prueba`.`gestionamodulo`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `prueba`.`gestionamodulo` (
-  `correoAdmin` VARCHAR(45) NOT NULL,
-  `id_modulo` INT NOT NULL,
-  PRIMARY KEY (`correoAdmin`, `id_modulo`),
-  INDEX `FK12` (`id_modulo` ASC) VISIBLE,
-  CONSTRAINT `FK11`
-    FOREIGN KEY (`correoAdmin`)
-    REFERENCES `prueba`.`administrador` (`correoAdmin`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-  CONSTRAINT `FK12`
-    FOREIGN KEY (`id_modulo`)
-    REFERENCES `prueba`.`modulo_info` (`id_modulo`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `prueba`.`mensaje`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `prueba`.`mensaje` (
-  `contenido` VARCHAR(45) NOT NULL,
-  `id_mensaje` INT NOT NULL,
-  `autor_` VARCHAR(45) NOT NULL,
-  `correoCliente` VARCHAR(45) NOT NULL,
-  `id_foro` INT NOT NULL,
-  PRIMARY KEY (`id_mensaje`, `correoCliente`, `id_foro`),
-  INDEX `FK4` (`correoCliente` ASC) VISIBLE,
-  INDEX `FK5` (`id_foro` ASC) VISIBLE,
-  CONSTRAINT `FK4`
-    FOREIGN KEY (`correoCliente`)
-    REFERENCES `prueba`.`cliente` (`correoCliente`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-  CONSTRAINT `FK5`
-    FOREIGN KEY (`id_foro`)
-    REFERENCES `prueba`.`foro` (`id_foro`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `prueba`.`participa`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `prueba`.`participa` (
-  `correoCliente` VARCHAR(45) NOT NULL,
-  `id_foro` INT NOT NULL,
-  PRIMARY KEY (`correoCliente`, `id_foro`),
-  INDEX `FK3` (`id_foro` ASC) VISIBLE,
-  CONSTRAINT `FK2`
-    FOREIGN KEY (`correoCliente`)
-    REFERENCES `prueba`.`cliente` (`correoCliente`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-  CONSTRAINT `FK3`
-    FOREIGN KEY (`id_foro`)
-    REFERENCES `prueba`.`foro` (`id_foro`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
