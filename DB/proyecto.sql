0170b9547ac0818e4429a468ddcf59d73b3ad0b3
mupe990216 <emunozp1400@alumno.ipn.mx>
2021-06-01 02:46:02 -0500

    ER

--- DB/proyecto.sql
+++ DB/proyecto.sql
@@ -1,19 +1,10 @@
--- MySQL Workbench Forward Engineering
+drop database if exists prueba;
+create database prueba;
+use prueba;
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+create table if not exists 
 
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
--- Schema prueba
--- -----------------------------------------------------
 
--- -----------------------------------------------------
--- Schema prueba
--- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `prueba` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
 USE `prueba` ;
 
